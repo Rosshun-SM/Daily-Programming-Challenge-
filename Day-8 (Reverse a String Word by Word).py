40efd8530b6eaba79eb1d7863fe32e29ed284da3
Rosshun-SM <rosshunsm@gmail.com>
2024-09-16 23:18:12 +0530

    Add files via upload

---
+++ Day-8 (Reverse a String Word by Word).py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 23:03:54 2024
+
+@author: Rosshun
+"""
+def reverse_words(s:str)->str:
+    s=s.strip()  # all leading and trailing spaces are removed
+    words=s.split() #converted to array with each word
+    reversed_words=words[::-1]  # words are reversed
+    return ' '.join(reversed_words)  # Words are joined into a single string with single space
+
+str=input("Input: ")  
+print("Output: ",reverse_words(str)) 
