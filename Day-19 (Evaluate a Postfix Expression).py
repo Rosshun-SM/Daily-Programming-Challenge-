9e202e671582077f06b368b76f42b9b7bb13779d
Rosshun-SM <rosshunsm@gmail.com>
2024-09-27 23:18:14 +0530

    Add files via upload

---
+++ Day-19 (Evaluate a Postfix Expression).py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 27 20:30:26 2024
+
+@author: Rosshun
+"""
+def evaluate_postfix(expression):
+    stack = []
+    tokens = expression.split()
+    
+    for token in tokens:
+        if token.lstrip('-').isdigit():  
+            stack.append(int(token))
+            
+        elif token in "+-*/":
+            b = stack.pop()
+            a = stack.pop()
+            
+            if token == '+':
+                stack.append(a + b)
+            elif token == '-':
+                stack.append(a - b)
+            elif token == '*':
+                stack.append(a * b)
+            elif token == '/':
+                stack.append(int(a / b))
+    
+    return stack[0]
+
+
+exp = input("Input: ")
+print("Ouput:", evaluate_postfix(exp))        
